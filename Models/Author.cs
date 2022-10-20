1cd74fcdca936375905b8430acb42cd994e20497
Phu Cuong <106647536+SkullCuong@users.noreply.github.com>
2022-10-20 21:29:43 +0700

    Validdata

--- Models/Author.cs
+++ Models/Author.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 public class Author {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Please type name of author !")]
     public string? Name { get; set; }
     public ICollection<Book>? Books { get; set; }
 }
